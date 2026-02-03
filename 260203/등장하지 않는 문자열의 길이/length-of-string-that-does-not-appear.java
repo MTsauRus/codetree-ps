35f71b86a1ea193a3f5ceed7beecf470401fab64
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-02-03 09:48:27 +0900

    Title: 등장하지 않는 문자열의 길이, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260203/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+
+        int low = 1;
+        int high = N;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (hasDuplicate(str, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+        
+        System.out.println(ans + 1);
+        
+    }
+
+    static boolean hasDuplicate(String str, int len) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i <= str.length() - len; i++) {
+            String sub = str.substring(i, i+len);
+
+            if (!set.add(sub)) {
+                return true;
+            }
+         }
+         return false;
+    }
+}
