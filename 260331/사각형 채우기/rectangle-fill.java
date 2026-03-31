9fb455cb2c60042843b35b228458e767ca957191
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-03-31 14:52:16 +0900

    Title: 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260331/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[1001];
+
+        dp[0] = 0; dp[1] = 1; dp[2] = 2; dp[3] = 3;
+
+        for (int i = 4; i < 1001; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+
+        System.out.print(dp[n]);
+
+    }
+}
