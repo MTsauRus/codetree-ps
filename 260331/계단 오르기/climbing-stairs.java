59c9aa69d0bf72d0cb810c1556f3c14ce4afc39c
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-03-31 09:06:50 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260331/계단 오르기/climbing-stairs.java
+++ 260331/계단 오르기/climbing-stairs.java
@@ -8,7 +8,7 @@ public class Main {
 
         dp[0] = 0; dp[1] = 0; dp[2] = 1; dp[3] = 1;
         for (int i = 4; i < 1001; i++) {
-            dp[i] = Math.max(dp[i-2]+1, dp[i-3]+1);
+            dp[i] = (dp[i-2]+1 + dp[i-3]+1) % 10007;
 
         }
         int N = Integer.parseInt(br.readLine());
