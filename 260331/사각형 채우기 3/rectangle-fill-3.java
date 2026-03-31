5604e8c4c12602e1ad29e0f0446e5c20c05a43be
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-03-31 16:53:56 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260331/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());        
+        
+
+        int[] dp = new int[1001];
+
+        dp[0] = 0; dp[1] = 2; dp[2] = 7;
+        for (int i = 3; i <= 1000; i++) {
+            dp[i] = (3 * dp[i-1] % MOD + dp[i-2] % MOD - dp[i-3] % MOD + MOD) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
