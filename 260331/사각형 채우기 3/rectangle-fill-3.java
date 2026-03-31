975b4fab8b56710546e7c0e3e17c8fab4b40550b
김성령 <107569343+MTsauRus@users.noreply.github.com>
2026-03-31 17:02:34 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260331/사각형 채우기 3/rectangle-fill-3.java
+++ 260331/사각형 채우기 3/rectangle-fill-3.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-        static long MOD = 1000000007;
+
         public static void main(String[] args) throws IOException {
+        int MOD = 1000000007;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());        
         
 
         int[] dp = new int[1001];
 
-        dp[0] = 0; dp[1] = 2; dp[2] = 7;
+        dp[0] = 1; dp[1] = 2; dp[2] = 7;
         for (int i = 3; i <= 1000; i++) {
-            dp[i] = (3 * dp[i-1] % MOD + dp[i-2] % MOD - dp[i-3] % MOD + MOD) % MOD;
+            dp[i] = (3 * dp[i-1] + dp[i-2] - dp[i-3]) % MOD;
         }
 
         System.out.println(dp[N]);
